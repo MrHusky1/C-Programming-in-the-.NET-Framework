439520ceceb34e2c13d3a27f456c1e96b0f62f8c
MrHusky1 <alper.dinler13@gmail.com>
2025-10-25 15:15:57 +0300

    Until 5.21 (21 excluded)

---
+++ APP/Domain/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP.Domain
+{
+    internal class Category
+    {
+    }
+}
